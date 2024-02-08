17f44bd93b7c6c76ca80b3fffc98feae692b9fa2
jiteen <jiteen.s@gmail.com>
2024-02-07 23:08:53 -0500

    Initial commit...

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hi, hello world....");
+        System.out.println("hi,This is project for jenkins deployment....");
+        System.out.println("Today's data is = "+ new Date());
+    }
+}
